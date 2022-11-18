71dcd3b544006e79746deb9fbb02536bea53f278
Francis Yip <francisyip7@gmail.com>
2022-11-18 19:15:18 +0000

    add extra test

--- things/models.py
+++ things/models.py
@@ -4,5 +4,5 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.CharField(max_length=120)
+    description = models.CharField(max_length=120 unique=False, blank=True)
     quantity = models.IntegerField(default=0, validators=[MaxValueValidator(100), MinValueValidator(0)])
